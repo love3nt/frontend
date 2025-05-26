1dc883d6f82d9a9de14f022de060a6e2ebbe5f7d
love3nt <146466803+love3nt@users.noreply.github.com>
2025-05-26 00:30:53 -0700

    Getting Setup

---
+++ components/TopLeftImg.js
@@ -0,0 +1,18 @@
+// next image
+import Image from "next/image";
+
+// This component renders an the paint splatter in the top left corner with specific styles
+const TopLeftImg = () => {
+  return ( 
+  <div className='absolute top-0 left-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] h-[200px] xl:h-[400px] opacity-50'>
+      <Image
+        src="/top-left-img.png"
+        alt=""
+        width={400}
+        height={400}
+      />
+      </div>
+  );
+};
+
+export default TopLeftImg;
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+// Next image
+import Image from 'next/image';
+
+// Components
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+
+// Framer Motion
+import { motion } from 'framer-motion';
+
+// Variants
+import { fadeIn } from '../variants';
+
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;

43bd6a36bebd6d437d8a0a688bc9b1f0f12898d5
love3nt <love3ntt@gmail.com>
2025-05-26 04:02:20 -0700

    Worked on customizing the homepage

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,56 @@ import { fadeIn } from '../variants';
 
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div className='bg-primary/60 h-full'>
+    {/* Text */}  
+      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+
+          {/* Title */}  
+          <motion.h1 
+            variants={fadeIn('down', 0.2)} 
+            initial="hidden" 
+            animate="show" 
+            exit='hidden'
+            className='h1'
+            
+            >
+              Crafting Cybersecurity Solutions <br /> for a {' '}<span className='text-accent'>Safer Tomorrow</span>
+          </motion.h1>
+
+          {/* Subtitle */}
+          <motion.p
+            variants={fadeIn('down', 0.3)} 
+            initial="hidden" 
+            animate="show" 
+            exit='hidden'
+            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
+
+            >
+              Passionate about exploring the frontiers of Cybersecurity. <br />Committed to building a secure digital world.
+          </motion.p>
+
+          {/* Projects Button */}
+          <div className='flex justify-center xl:hidden relative'>
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn('down', 0.4)} 
+            initial="hidden" 
+            animate="show" 
+            exit='hidden'
+            className='hidden xl:flex' 
+
+            >
+            <ProjectsBtn />
+          </motion.div>
+
+          </div>
+        </div>
+      </div>
+
+    {/* Image */}  
+
 };
 
 export default Home;
